06d390befbc2117d0a9de7664a4ae628ea1d4805
Bothaina-ahmed <boossyyahmed1997@gmail.com>
2020-12-24 20:15:29 +0200

    javascript

--- Assignments js/Even&Odd js/main.js
+++ Assignments js/Even&Odd js/main.js
@@ -2,9 +2,9 @@ var x= parseFloat(prompt("Enter number"));
 var y= x%2;
 if(x%2==0)
 {
-    console.log("Even Number"+y);
+    console.log("Even Number"+x);
 }
 if(x%2==1)
 {
-    console.log("Odd Number"+y)
+    console.log("Odd Number"+x)
 }
